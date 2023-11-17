f8230115fb4622c2882cd4023f2d63425b2339fb
Marco <marco1.delvecchio@gmail.com>
2023-11-17 18:19:54 +0100

    Modified add post page to match figma design

--- lib/feature_notes/presentation/add_post_page.dart
+++ lib/feature_notes/presentation/add_post_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AddPost extends StatefulWidget {
+  const AddPost({super.key});
+
   @override
-  _AddPostState createState() => _AddPostState();
+  State<AddPost> createState() => _AddPostState();
 }
 
 class _AddPostState extends State<AddPost> {
-  final _formKey = GlobalKey<FormState>();
   String _title = '';
   String _body = '';
 
@@ -14,20 +15,48 @@ class _AddPostState extends State<AddPost> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {
+            // Add attach logic
+          }, icon: const Icon(Icons.attach_file))
+        ],
         title: Text('Add Post'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Container(
-              color: Colors.grey[300],
-              child: const Center(
-                child: Text('Add Post content goes here'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              style: const TextStyle(fontSize: 48),
+              // Set text style to match hint style
+              decoration: const InputDecoration(
+                enabledBorder: InputBorder.none,
+                hintText: 'Titolo',
+                hintStyle: TextStyle(fontSize: 48),
               ),
+              onChanged: (value) {
+                setState(() {
+                  _title = value;
+                });
+              },
             ),
-          ),
-        ],
+            TextFormField(
+              style: const TextStyle(fontSize: 24),
+              // Set text style to match hint style
+              decoration: const InputDecoration(
+                enabledBorder: InputBorder.none,
+                hintText: 'Scrivi qualcosa',
+                hintStyle: TextStyle(fontSize: 24),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _body = value;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
